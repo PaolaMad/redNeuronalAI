871991ed26eaa834bc288758694f32fe5aea0faa
PaolaMad <paolamadridt@gmail.com>
2024-12-05 13:38:27 -0600

    html y servidor

---
+++ README.md
@@ -0,0 +1,11 @@
+# Descripcion 
+Este proyecto implementa una red neuronal utilizando TensorFlow para leer numeros. Esta red neuronal es capaz de leer numeros no digitalizados, reconociendo trazos dibujados a traves del conjunto de datos MNIST (Modified National Institute of Standards and Technology).
+
+```bash
+# Para instalar dependencias
+pip install tensorflow
+
+# Para clonar repositorio
+git clone https://github.com/PaolaMad/redNeuronalAI.git
+```
+
--- numeros.py
+++ numeros.py
@@ -5,25 +5,19 @@ import tensorflow_datasets as tfds
 
 import math
 import numpy as np
-import matplotlib.pyplot as plt
 import logging
-logger = tf.get_logger()
 
+from urllib import parse
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
 
-
+# Carga de datos de MNIST (set de entrenamiento y de pruebas)
 dataset, metadata = tfds.load('mnist', as_supervised=True, with_info=True)
 train_dataset, test_dataset = dataset['train'], dataset['test']
 
-class_names = [
-    'Cero', 'Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco', 'Seis',
-    'Siete', 'Ocho', 'Nueve'
-]
-
-num_train_examples = metadata.splits['train'].num_examples
-num_test_examples = metadata.splits['test'].num_examples
-
-#Normalizar: Numeros de 0 a 255, que sean de 0 a 1
+# Normalizar: Números de 0 a 255, que sean de 0 a 1
 def normalize(images, labels):
     images = tf.cast(images, tf.float32)
     images /= 255
@@ -32,82 +26,69 @@ def normalize(images, labels):
 train_dataset = train_dataset.map(normalize)
 test_dataset = test_dataset.map(normalize)
 
-#Estructura de la red
+# Estructura de la red
 model = tf.keras.Sequential([
-	tf.keras.layers.Flatten(input_shape=(28,28,1)),
-	tf.keras.layers.Dense(64, activation=tf.nn.relu),
-	tf.keras.layers.Dense(64, activation=tf.nn.relu),
-	tf.keras.layers.Dense(10, activation=tf.nn.softmax) #para clasificacion
+    tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+    tf.keras.layers.Dense(64, activation=tf.nn.relu),
+    tf.keras.layers.Dense(64, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)  # Para clasificación
 ])
 
-#Indicar las funciones a utilizar
+# Configurar el modelo
 model.compile(
-	optimizer='adam',
-	loss='sparse_categorical_crossentropy',
-	metrics=['accuracy']
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
 )
 
-#Aprendizaje por lotes de 32 cada lote
+# Entrenamiento
 BATCHSIZE = 32
-train_dataset = train_dataset.repeat().shuffle(num_train_examples).batch(BATCHSIZE)
+train_dataset = train_dataset.repeat().shuffle(metadata.splits['train'].num_examples).batch(BATCHSIZE)
 test_dataset = test_dataset.batch(BATCHSIZE)
 
-#Realizar el aprendizaje
 model.fit(
-	train_dataset, epochs=5,
-	steps_per_epoch=math.ceil(num_train_examples/BATCHSIZE) #No sera necesario pronto
+    train_dataset, epochs=5,
+    steps_per_epoch=math.ceil(metadata.splits['train'].num_examples / BATCHSIZE)
 )
 
-#Evaluar nuestro modelo ya entrenado, contra el dataset de pruebas
-test_loss, test_accuracy = model.evaluate(
-	test_dataset, steps=math.ceil(num_test_examples/32)
-)
+# Clase para definir el servidor HTTP
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
-print("Resultado en las pruebas: ", test_accuracy)
+    def do_GET(self):
+        # Respuesta básica para solicitudes GET
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.send_header("Access-Control-Allow-Origin", "*")  # Evitar problemas con CORS
+        self.end_headers()
+        self.wfile.write(b"Servidor funcionando. Usa POST para enviar datos.")
 
+    def do_POST(self):
+        print("Petición recibida")
 
-for test_images, test_labels in test_dataset.take(1):
-	test_images = test_images.numpy()
-	test_labels = test_labels.numpy()
-	predictions = model.predict(test_images)
+        # Obtener datos de la petición y limpiar los datos
+        content_length = int(self.headers['Content-Length'])
+        data = self.rfile.read(content_length)
+        data = data.decode().replace('pixeles=', '')
+        data = parse.unquote(data)
 
-def plot_image(i, predictions_array, true_labels, images):
-	predictions_array, true_label, img = predictions_array[i], true_labels[i], images[i]
-	plt.grid(False)
-	plt.xticks([])
-	plt.yticks([])
+        # Transformar datos para que coincidan con el formato MNIST
+        arr = np.fromstring(data, np.float32, sep=",")
+        arr = arr.reshape(28, 28)
+        arr = np.array(arr)
+        arr = arr.reshape(1, 28, 28, 1)
 
-	plt.imshow(img[...,0], cmap=plt.cm.binary)
+        # Realizar y obtener la predicción
+        prediction_values = model.predict(arr, batch_size=1)
+        prediction = str(np.argmax(prediction_values))
+        print("Predicción final: " + prediction)
 
-	predicted_label = np.argmax(predictions_array)
-	if predicted_label == true_label:
-		color = 'blue'
-	else:
-		color = 'red'
+        # Responder a la petición HTTP
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+        self.wfile.write(prediction.encode())
 
-	plt.xlabel("Prediccion: {}".format(class_names[predicted_label]), color=color)
-
-def plot_value_array(i, predictions_array, true_label):
-	predictions_array, true_label = predictions_array[i], true_label[i]
-	plt.grid(False)
-	plt.xticks([])
-	plt.yticks([])
-	thisplot = plt.bar(range(10), predictions_array, color="#888888")
-	plt.ylim([0,1])
-	predicted_label = np.argmax(predictions_array)
-
-	thisplot[predicted_label].set_color('red')
-	thisplot[true_label].set_color('blue')
-
-numrows=5
-numcols=3
-numimages = numrows*numcols
-
-plt.figure(figsize=(2*2*numcols, 2*numrows))
-for i in range(numimages):
-	plt.subplot(numrows, 2*numcols, 2*i+1)
-	plot_image(i, predictions, test_labels, test_images)
-	plt.subplot(numrows, 2*numcols, 2*i+2)
-	plot_value_array(i, predictions, test_labels)
-
-plt.show()
+# Iniciar el servidor en el puerto 8000
+print("Iniciando el servidor...")
+server = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
